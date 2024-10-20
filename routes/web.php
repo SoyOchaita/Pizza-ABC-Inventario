05625cbb35c8b84cc1498b4f6d0dc3626246afe5
Pc Alfonso Ochaita <ochaita2404@gmail.com>
2024-10-19 20:34:48 -0600

    Mis cambios locales antes de hacer pull

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('productos', [ProductController::class, 'index'])->name('productos.index'); // Ruta para listar productos
 Route::get('productos/{id}', [ProductController::class, 'show'])->name('productos.show'); // Ruta para mostrar detalles de un producto
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./**/*.blade.php'],
-    darkMode: 'class',
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', 'Montserrat','sans-serif', ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-
-            fontWeight: {
-                normal: '400',
-                extrabold: '800',
-            },
-            colors: {
-                stone: {
-                    900: '#1c1917',
-                    800: '#292524'
-                  }
-                
-              }
         },
     },
-    plugins: [],
+
+    plugins: [forms],
 };
